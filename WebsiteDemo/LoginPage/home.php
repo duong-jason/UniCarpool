410d0a65da13a2195090cbc646bd95f4372071c6
Kevin Nguyen <60242697+kevinnguyen57@users.noreply.github.com>
2022-07-01 10:49:31 -0700

    Create home.php

---
+++ WebsiteDemo/LoginPage/home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home Page</title>
+  <link rel="stylesheet" type="text/css" href="stylehome.css">
+</head>
+<body>
+  <div style="position:fixed;width:1070px;height:109px;left:0px;top:0px;background: #667684;">
+  </div>
+
+  <div
+style="box-sizing: border-box;position: absolute;width: 447px;height: 1024px;left: 1070px;top: 0px;background: #667684;border: 1px solid #000000;">
+  </div>
+
+  <div pic="logo">
+    <img src="UniCarpoolLogo1.png">
+  </div>
+
+  <div class="hero">
+      <h1 class = "hero-title">UniCarpool</h1>
+      <h1 class = "hero-name">Kevin</h1>
+      <h1 class = "hero-friend">Friends</h1>
+      <h1 class = "hero-home">Home to view post</h1>
+  </div>
+
+  <div class="navbar">
+    <a href="home.php">Home</a>
+    <a href="post.php">Post</a>
+    <a href="groups.php">Groups</a>
+  </div>
+  
+  <div>
+    <img src="doggo.jpg" style="box-sizing:borderbox;border-radius:50%;position:absolute;width:172px;height:164px;left:1208px;top:26px;border:3px solid #000000;"
+  </div>
+
+  <hr style="position: absolute;width:408.01px;height:0px;left:1090px;top:238.14px;border:2px solid #FFFFFF;transform: rotate(-0.3deg);">
+
+  <hr style= "position:absolute;width:408.01px;height:0px;left:1090px;top: 832.14px;border: 2px solid #FFFFFF;transform: rotate(-0.3deg);">
+
+  <hr style= "position:absolute;width:408.01px;height:0px;left:1090px;top:924.14px;border: 2px solid #FFFFFF;transform: rotate(-0.3deg);">
+
+  <div class="other">
+    <a href="settings.php">Settings</a>
+    <a href="login.php">Logout</a>
+  </div>
+  
+</body>
+</html>
